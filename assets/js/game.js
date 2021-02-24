1b1ba660a63e8b004e03c099457c7447610d48ef
Micheal Ellis <mellis.eriecon@gmail.com>
2021-02-23 23:51:35 -0500

    added final pages/html/js/css

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,14 +67,14 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem('mostRecentScore', score)
 
-    return window.location.assign("/end.html")
+    return window.location.assign("../endpage/end.html")
 
   }
 
   questionCounter++
   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-  progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-  
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
 
   const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
   currentQuestion = availableQuestions[questionsIndex]
